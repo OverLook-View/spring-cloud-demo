449dcfd2041b711ddb43c4ef35f57d298b754d26
sheny <admin@DESKTOP-FFLSL2H>
2018-04-04 16:50:59 +0800

    spring cloud demo parent

---
+++ spring-cloud-demo/spring-cloud-demo-eureka/src/main/java/com/example/sy/spring/cloud/demo/eureka/springclouddemoeureka/SpringCloudDemoEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.example.sy.spring.cloud.demo.eureka.springclouddemoeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoEurekaApplication.class, args);
+	}
+}
